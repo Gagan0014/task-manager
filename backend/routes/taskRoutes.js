c1c4f1df8fdb6eac8e01955dff0ec9b30bac16ad
Gagan0014 <gaganpathak2238@gmail.com>
2026-04-06 02:42:15 +0530

    delete api added

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -44,4 +44,17 @@ router.put('/task/:id',async(req,res)=>{
     }
 });
 
+// now delete api 
+router.delete('/task/:id',async(req,res)=>{
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if(!task){
+            return res.status(404).json({message:"Task not found"});
+        }
+        res.json({message:"task deleted successfully"});
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+});
+
 export default router
