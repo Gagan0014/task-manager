931f844ef906eb371e7b9298cae661d0df7f9e78
Gagan0014 <gaganpathak2238@gmail.com>
2026-04-07 04:25:16 +0530

    added auth login controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from '../models/user.js'
 import asyncHandler from '../utils/asyncHandler.js'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const register = asyncHandler(async (req,res)=>{
     const {email,password,name} = req.body
@@ -24,4 +25,31 @@ export const register = asyncHandler(async (req,res)=>{
         success:true,
         message:"user registered successfully"
     })
+})
+
+export const login = asyncHandler(async (req,res)=>{
+    const {email,password} = req.body;
+    if(!email || !password){
+        throw new Error("All entries required")
+    }
+
+    const user = await User.findOne({email});
+    if(!user){
+        throw new Error("user doesn't exist : Register first")
+    }
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(!isMatch){
+        throw new Error("Invalid Credentials")
+    }
+    const token = jwt.sign(
+        {id:user.id},
+        process.env.JWT_SECRET,
+        {expiresIn:'1d'}
+    )
+
+    res.json({
+        success:true,
+        token
+    })
+
 })
