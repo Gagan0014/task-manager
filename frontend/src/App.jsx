72422c9e804272ab5f42c057093c196f8f84f56b
Gagan0014 <gaganpathak2238@gmail.com>
2026-04-27 16:40:27 +0530

    started frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
-import Auth from "./components/Auth";
-import Dashboard from "./components/Dashboard";
-
 function App() {
-  const [token, setToken] = useState(localStorage.getItem("token"));
-
-  function logout() {
-    localStorage.removeItem("token");
-    setToken(null);
-  }
-
-  if (!token) {
-    return <Auth setToken={setToken} />;
-  }
-
-  return <Dashboard token={token} onLogout={logout} />;
+  return (
+    <h1 className="text-red-500 text-4xl font-bold">
+      Tailwind Working here
+    </h1>
+  );
 }
 
 export default App;
